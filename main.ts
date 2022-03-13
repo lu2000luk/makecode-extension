395b62809c1c861369d19f472693c6d60eb21c0c
lu2000luk <81090607+lu2000luk@users.noreply.github.com>
2022-03-13 18:30:52 +0100

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,20 @@
+//% color="#3e0ffa" weight=100
+namespace luk {
+    //% block name="estenzione inutile creata da lu2000luk"
+    export function herld() {
+
+    }
+
+    //% block name="fatto!"
+    export function seTwo() {
+
+    }
+    //% block name="da finire"
+    export function lCaseTwo() {
+
+    }
+    //% block name="commento"
+    export function wo() {
+
+    }
+}
